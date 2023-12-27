46ac53c35b3e99e2ad70ff7fb7ee0497c91af2aa
geiger01 <tantangeiger@gmail.com>
2023-12-27 21:40:23 +0200

    basic course page created with erdirect to lessons page

---
+++ src/app/[courseId]/lessons/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from 'next/navigation';
+
+async function getFirstLesson(courseId: string) {
+    const res = await fetch(`https://lecturekit-new-git-dev-geiger01.vercel.app/api/v1/courses/${courseId}/lessons`, {
+        cache: 'no-store',
+        headers: {
+            'x-api-key': process.env.LECTUREKIT_API_KEY || ''
+        }
+    });
+
+    const data = await res.json();
+
+    if (!data.success) {
+        throw new Error(data.message);
+    }
+
+    return data.data?.results?.[0];
+}
+
+export default async function LessonsPage({ params }: { params: { courseId: string; lessonId: string; }; }) {
+    const lesson = await getFirstLesson(params.courseId);
+    if (!lesson) {
+        redirect(`/${params.courseId}`);
+    }
+    redirect(`/${params.courseId}/lessons/${lesson.id}`);
+}
