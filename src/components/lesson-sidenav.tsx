6ca09ad7600048414750082c1d16338e5162a93e
geiger01 <tantangeiger@gmail.com>
2024-01-01 21:57:52 +0200

    lesson content sidenav done

--- src/components/lesson-sidenav.tsx
+++ src/components/lesson-sidenav.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useParams, useRouter } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { Button } from './ui/button';
 import { Menu, X } from 'lucide-react';
+import Link from 'next/link';
 
 interface ILessonSidenavProps {
     course: any;
@@ -12,7 +13,6 @@ interface ILessonSidenavProps {
 
 export const LessonSidenav = ({ course }: ILessonSidenavProps) => {
     const [menuOpen, setMenuOpen] = useState(false);
-    const router = useRouter();
     const { courseId, lessonId } = useParams();
 
     return (
@@ -32,15 +32,24 @@ export const LessonSidenav = ({ course }: ILessonSidenavProps) => {
                 }
             </Button>
             <aside className={
-                cn("fixed bottom-0 lg:sticky lg:h-[calc(100vh_-_64px)] z-20 bg-background  overflow-hidden top-[64px] border-r min-w-[290px] max-w-[290px] grid grid-cols-[1fr] grid-rows-[1fr] md:grid-rows-[1fr_auto] transition-all",
+                cn("fixed bottom-0 lg:sticky lg:h-[calc(100vh_-_64px)] z-20 bg-background  overflow-hidden top-[64px] border-r min-w-[290px] max-w-[290px] grid grid-rows-[auto_1fr] transition-all",
                     menuOpen ? `translate-x-[0] lg:sticky` : `translate-x-[-100%] lg:translate-x-[0]`
                 )}
             >
-                <div className="flex h-full flex-col items-center gap-2 overflow-auto p-2 md:p-3">
-
-                </div>
-                <div className="hidden md:block mx-auto my-3 w-full px-5">
-
+                <p className='font-semibold border-b mx-5 py-3 bg-white'>Course content</p>
+                <div className="flex h-full flex-col overflow-auto pt-3">
+                    {course.chapters.map((chapter: any, idx: number) => (
+                        <div className=' text-gray-800 px-5' key={chapter.id}>
+                            <p className='font-semibold mb-1'>Chapter {idx + 1}: {chapter.name}</p>
+                            <div className='pb-5 flex flex-col gap-[2px]'>
+                                {chapter.lessons.map((lesson: any, lessonIdx: number) => (
+                                    <Link href={`/${courseId}/lessons/${lesson.id}`} className={cn('text-sm cursor-pointer font-light text-gray-500 hover:bg-blue-50 p-2 rounded-sm', lesson.id === lessonId ? 'bg-blue-50' : '')} key={lesson.id}>
+                                        {lessonIdx + 1}: {lesson.name}
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </aside>
         </>
